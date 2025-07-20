2e1af87ae4a0b3f5df481901b99ec3ac205cee0c
Jerren Shaffer <88438765+Texasgold88@users.noreply.github.com>
2025-07-20 06:55:02 -0500

    Update App.py

--- Backend/App.py
+++ Backend/App.py
@@ -1,1 +1,13 @@
+from flask import Flask, request, jsonify
+import json
 
+app = Flask(__name__)
+
+@app.route("/submit", methods=["POST"])
+def submit():
+    data = request.get_json()
+    print("Received:", data)
+    return jsonify({"status": "success", "message": "Data received."})
+
+if __name__ == "__main__":
+    app.run(debug=True)
